4d99eb991e713461d763a908588464027674fb60
Pohuș Tabita <emanuela.pohus@emanuel.ro>
2024-01-22 09:38:29 -1200

    Problema1

---
+++ Examen/Pers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class Pers
+    {
+        public  string FirstName { get; set; }
+        public  string LastName { get; set; }
+        public int YearOfBirth { get; set; }
+
+        public int Age
+        {
+            get { return DateTime.Now.Year - YearOfBirth; }
+        }
+
+        static class PersonProcessor
+        {
+            public static Person GetOldestPerson(Person[] people)
+            {
+                Person oldestPerson = people[0];
+                foreach (Person person in people)
+                {
+                    if (person.Age > oldestPerson.Age)
+                    {
+                        oldestPerson = person;
+                    }
+                }
+                return oldestPerson;
+            }
+
+        }
+       
+    }
+}
---
+++ Examen/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Examen
+{
+    class Person
+    {
+        public string? FirstName { get; private set; }
+        public int Age { get; internal set; }
+        public string LastName { get; private set; }
+        public int YearOfBirth { get; private set; }
+
+        class Program
+        {
+            public static object PersonProcessor { get; private set; }
+
+            static void Main()
+            {
+                Console.Write("Numarul de persoane: ");
+                int n = int.Parse(Console.ReadLine());
+
+                Person[] persoane = new Person[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine($"Introduceti informatiile pentru persoana :");
+
+                    Console.Write("Prenume: ");
+                    string prenume = Console.ReadLine();
+
+                    Console.Write("Nume de familie: ");
+                    string numeFamilie = Console.ReadLine();
+
+                    Console.Write("Anul nasterii: ");
+                    int anNastere = int.Parse(Console.ReadLine());
+
+                    persoane[i] = new Person
+                    {
+                        FirstName = prenume,
+                        LastName = numeFamilie,
+                        YearOfBirth = anNastere
+                    };
+                }
+                Person ceaMaiInVarsta = PersonProcessor.GetOldestPerson(persoane);
+
+                // Afișarea informațiilor despre cea mai în vârstă persoană
+                if (ceaMaiInVarsta != null)
+                {
+                    Console.WriteLine($"\nCea mai în vârstă persoană este: {ceaMaiInVarsta.FirstName} {ceaMaiInVarsta.LastName}, cu vârsta de {ceaMaiInVarsta.Age} ani.");
+                }
+                else
+                {
+                    Console.WriteLine("\nNu exista persoane pentru a determina cea mai in varsta persoana.");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+
+    
+  
